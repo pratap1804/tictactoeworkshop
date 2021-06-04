04b984b67e8fbf902564c9006d98d58926784193
Pratap <pratap.1804@gmail.com>
2021-06-04 09:47:00 +0530

    Same Master file is copied here

---
+++ tictactoeUC1.java
@@ -0,0 +1,13 @@
+public class tictactoe{
+/**
+Displaying GameBoard
+*/
+public static void main( String[] args )
+    {
+        char[][] gameBoard= {{' ','|',' ','|',' '},
+        		{'-','+','-','+','-'},
+        		{' ','|',' ','|',' '},
+        		{'-','+','-','+','-'},
+        		{' ','|',' ','|',' '}};
+	}
+}
